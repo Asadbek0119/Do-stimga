0ccaad3d2f3b9cb66125306e793065afee0c6ee4
Do'st <asadbekxudaynazarov0@gmail.com>
2023-01-30 18:02:09 +0500

    end

---
+++ main.js
@@ -0,0 +1,6 @@
+let ism = prompt("Isminggizni kiriting")
+
+while(ism !== 'Mashxura 2003') {
+    ism = prompt('Parol xato: qaytatan kiriting yo telfoni egasiga bering')
+}
+alert('Salom Mashxura Turatova tugulgan kuninggiz bilan chin qalbimdan tabriklayman')
